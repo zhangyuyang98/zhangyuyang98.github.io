3f25582a3a6548912a35b9822efe79de950f63e5
zhangyuyang98 <zhangyuyang@uchicago.edu>
2024-04-04 20:38:10 -0500

    1

--- starter/starter/js/sample.js
+++ starter/starter/js/sample.js
@@ -2,3 +2,30 @@ function howdy() {
   var firstName = prompt("Hello. What is your first name?");
   alert("Howdy " + firstName + "!");
 }
+
+//Simple for loop.
+function simpleFor() {
+  for (let i = 0; i < 5; i++) {
+    console.log("The number is "+i)
+  }
+}
+
+function letScope() {
+  let x = 1;
+  if(x===1){
+    let x = 2;
+    console.log("inside x = "+x);
+  };
+  console.log("outside x = "+x);
+};
+
+function parentFunction()
+{
+  let a = 1;
+  function childFunction()
+  {
+    var b = a + 2;
+    return b;
+  }
+  return childFunction();
+}
